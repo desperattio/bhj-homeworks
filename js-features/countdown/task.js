749f08d1f0b70c322c6c660fab0b75bda7c3bfc7
katherine <ek.gvozd@yandex.ru>
2023-11-19 17:29:28 +0300

    Повышенная сложность 1 у таймера

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,10 @@
-const timer = document.getElementById("timer");
+const timer = document.getElementById("timer__seconds");
 
 const intervalId = setInterval(() => {
     timer.textContent--;
+    timer.textContent = Number(timer.textContent) < 10 ? '0' + timer.textContent : timer.textContent;
     // остановка таймера, если дошли до 0
-    if (timer.textContent === '0') {
+    if (timer.textContent === '00') {
         clearInterval(intervalId);
         alert("Вы победили в конкурсе!");
     }
